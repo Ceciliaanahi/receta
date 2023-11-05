df6bae39bc264e566c1426db8c30a2be38a61684
Ceciliaanahi <144741761+Ceciliaanahi@users.noreply.github.com>
2023-11-05 12:51:48 -0300

    Add files via upload

---
+++ Js/java.js
@@ -0,0 +1,5 @@
+function pop() {
+  alert("Cecilia Anahí Ramos (2923) 451247 cecianahi09@hotmail.com");
+        }
+function modonocturno() {document.body.style.backgroundColor="black";}
+function modonocturno1 (){document.body.style.backgroundColor="white";}
